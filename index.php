0c8c8cb6317ba6fca92e301e4d3fe59b16510b33
Julimar Junior <julimargomesjr@hotmail.com>
2021-02-20 13:25:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 	include 'functions.php';
 
-	// Criar URL encurtada
-		shortUrl('https://google.com', 'google');
-
-	// Abrir URL encurtada
-		openShortUrl(clearShortUrl($_SERVER["REQUEST_URI"]));
+	openShortUrl(clearShortUrl($_SERVER["REQUEST_URI"]));
